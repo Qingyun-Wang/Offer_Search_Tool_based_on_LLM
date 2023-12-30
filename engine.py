e86e2c6a32155199ed65f58613dfeb29e6f35df8
qingyun <qwang63@uic.edu>
2023-12-30 17:31:43 -0600

    update the complete and clean data processing file and some minor modify of others

--- engine.py
+++ engine.py
@@ -60,31 +60,36 @@ def get_embedding(text):
     # Convert to one-dimensional array and return
     return embeddings.squeeze().numpy()
 
-def input_prep(txt,stop_words):
-    txt = re.sub(r'\$?\d+', '', txt)
-    txt = txt.replace("'","").replace("$","").replace('-'," ")
 
+def clean_string(text):
+    # Replace any character that is not a letter or a space with nothing
+    cleaned_text = re.sub(r'[^a-zA-Z\s]', ' ', text)
+    return cleaned_text
+
+# prepare score for bert similarity
+def input_prep_with_embedding(txt,stop_words):
+
+    txt =  clean_string(txt)
     words = word_tokenize(txt)
     # words =[word.strip(u"\u2122").strip(u'\u0256') for word in words]
     filtered_text = [word for word in words if (not word.lower() in stop_words) and (word.isalpha())]
     tagged = pos_tag(filtered_text)
     nouns = [word for word, pos in tagged if pos in ['NN', 'NNS', 'NNP', 'NNPS']]
-    
+    txt = " ".join(nouns)
     txt = get_embedding(txt)
-    
     txt=reshape_to_2d(txt)
 
     return txt
+
 
 def rule_prediction(row, txt):
     score = cosine_similarity(txt,reshape_to_2d(row['training_str_vector']))[0][0]
     return score
 
-
+# preprocessing text for fuzzywuzzy similarity
 def input_prep_fz(txt,stop_words):
-    txt = re.sub(r'\$?\d+', '', txt)
-    txt = txt.replace("'","").replace("$","").replace('-'," ")
 
+    txt =  clean_string(txt)
     words = word_tokenize(txt)
     # words =[word.strip(u"\u2122").strip(u'\u0256') for word in words]
     filtered_text = [word for word in words if (not word.lower() in stop_words) and (word.isalpha())]
@@ -92,14 +97,29 @@ def input_prep_fz(txt,stop_words):
     nouns = [word for word, pos in tagged if pos in ['NN', 'NNS', 'NNP', 'NNPS']]
     return " ".join(nouns)
 
+# fuzzywuzzy similarity
 def rule_prediction_fz(row, txt):
     score = fuzz.partial_ratio(txt.upper(),str(row['training_str']).upper())
     return score
 
-
+"""
 def ensemble_scores(score1, score2):
     # Apply a nonlinear transformation to accentuate extreme values
     transformed_score1 = score1 ** 2
+    transformed_score2 = score2 ** 2
+
+    # Compute the average of the transformed scores
+    combined_score = (transformed_score1 + transformed_score2) / 2
+
+    # Apply the inverse of the transformation to bring the scale back to [0, 1]
+    combined_score = combined_score ** 0.5
+
+    return combined_score
+"""
+
+def ensemble_scores(score1, score2):
+    # Apply a nonlinear transformation to accentuate extreme values
+    transformed_score1 = (score1/100) ** 2
     transformed_score2 = score2 ** 2
 
     # Compute the average of the transformed scores
@@ -121,7 +141,7 @@ def run_engine(df, input_str, num_results=20):
 
     df['pred_score_fz'] = df.apply(lambda row: rule_prediction_fz(row, txt_fz),axis=1)
 
-    txt = input_prep(input_str,sw)
+    txt = input_prep_with_embedding(input_str,sw)
 
     df['pred_score_bert'] = df.apply(lambda row: rule_prediction(row, txt),axis=1)
 
@@ -129,5 +149,5 @@ def run_engine(df, input_str, num_results=20):
 
     results = df.sort_values(by ='combined_score',ascending=False)[['OFFER',"BRAND","RETAILER","combined_score"]].head(num_results)
     
-
-    print(results[['OFFER',"BRAND","RETAILER", 'combined_score']].to_string(index=False))
+    #print(results[['OFFER',"BRAND","RETAILER", 'combined_score']].to_string(index=False))
+    return results[['OFFER',"BRAND","RETAILER", 'combined_score']]
