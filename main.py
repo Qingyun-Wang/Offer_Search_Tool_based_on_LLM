fce8013d637b579013d7bee62f65ff8dea735d05
qingyun <qwang63@uic.edu>
2024-01-21 11:56:30 -0600

    use streamlit to make a web app

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from engine import * 
 from database import *
 import numpy as np
+import streamlit as st
+
+
+def main():
+    offer_data = query_db()
+    
+    # Create a text input widget in Streamlit
+    input_text = st.text_input("What do you want to search:")
+    
+    # Run the search engine when input is provided
+    if input_text:
+        result = run_engine(offer_data, input_text, 20)
+        st.write(result)  # Display the result
 
 if __name__ == "__main__":
-    offer_data = query_db()
-    input_text = input("what do you want to search: ")
-    result = run_engine(offer_data, input_text, 20)
-    print(result)
+    main()
